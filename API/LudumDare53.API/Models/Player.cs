dcf9d64cfd164a8df86dfa0d50a3e37e88abb9c5
Christopher Plummer <chrisplummer0194@gmail.com>
2023-04-30 19:11:00 -0400

    Use PlayerDto

--- API/LudumDare53.API/Models/Player.cs
+++ API/LudumDare53.API/Models/Player.cs
@@ -6,4 +6,14 @@ public class Player : Resource
     public int DaysCompleted { get; set; }
     public int TotalMoneyEarned { get; set; }
     public int DeliveriesMade { get; set; }
+
+    public Player() { }
+
+    public Player(PlayerDto playerDto)
+    {
+        Name = playerDto.Name;
+        DaysCompleted = playerDto.DaysCompleted;
+        TotalMoneyEarned = playerDto.TotalMoneyEarned;
+        DeliveriesMade = playerDto.DeliveriesMade;
+    }
 }
